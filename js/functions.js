5b8a6e247bc6b1fa9281305dd0bef85f0e9de218
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-02-26 19:16:14 +0300

    finalfinalfix

--- js/functions.js
+++ js/functions.js
@@ -3,58 +3,15 @@
 const isStringLengthCorrect = (string, length) => (string.length <= length);
 
 // Функция для проверки, является ли строка палиндромом
-const flipsTheString = (string) => {
-  let newString = '';
-
-  for (let i = 0; i < string.length; i++) {
-    newString = newString + string[string.length - 1 - i];
-  }
-  return newString;
-};
-
-const removesSpaces = (string) => string.replaceAll(' ','');
-
-const removesSpacesSplitJoin = (string) => string.split(' ').join('');
 
 const isPalindrome = (string) => {
-  const stringLowerCase = removesSpaces(string).toLowerCase();
-  return stringLowerCase === flipsTheString(stringLowerCase);
+  const stringLowerCaseNoSpace = string.split(' ').join('').toLowerCase();
+  return stringLowerCaseNoSpace === stringLowerCaseNoSpace.split('').reverse().join('');
 };
-
-const isPalindromeSplitReverseJoin = (string) => {
-  const stringLowerCase = removesSpaces(string).toLowerCase();
-  return stringLowerCase === stringLowerCase.split('').reverse().join('');
-};
-// Оставить один вариант?
 
 // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа
 
-const getNumberInString = function(string) {
-  let numberInString = '';
-  for (let i = 0; i < string.length; i++) {
-    if (isNaN(Number(string[i]))) {
-      continue;
-    }
-    numberInString = numberInString + string[i];
-  }
-  return (numberInString === '') ? NaN : Number(numberInString);
-};
-// Если хотите усложнить задание, предусмотрите случай, когда вместо строки приходит число:
-const getNumberInStringAdvanced = function(numberString) {
-  const string = numberString.toString();
-  let numberInString = '';
-  for (let i = 0; i < string.length; i++) {
-    if (isNaN(Number(string[i]))) {
-      continue;
-    }
-    numberInString = numberInString + string[i];
-  }
-  return (numberInString === '') ? NaN : Number(numberInString);
-};
-
-const getNumberInStringRealAdvanced = (numberString) => numberString.toString().replaceAll(/\D/g, '');
-// можно указать [^0-9] есть ли разница?
-
+const getNumberInString = (numberString) => (numberString.toString().replaceAll(/\D/g, '')) ? numberString.toString().replaceAll(/\D/g, '') : NaN;
 
 // Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки. Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.
 const addSymbol = (addableLength, symbols) => {
@@ -72,20 +29,8 @@ const addSymbol = (addableLength, symbols) => {
 
 const formatStringToPattern = (string, minLength, symbols) => (string.length >= minLength) ? string : addSymbol(minLength - string.length , symbols) + string;
 
-const formatStringToPatternPadStart = (string, minLength, symbols) => {
-  const moduloLength = (minLength - string.length) % symbols.length;
-  if (moduloLength) {
-    return symbols.slice(0,moduloLength) + string.padStart(minLength - moduloLength,symbols);
-  }
-  return string.padStart(minLength ,symbols);
-};
-
 isStringLengthCorrect();
-removesSpacesSplitJoin();
 isPalindrome();
-isPalindromeSplitReverseJoin();
 getNumberInString();
-getNumberInStringAdvanced();
-getNumberInStringRealAdvanced();
 formatStringToPattern();
-formatStringToPatternPadStart();
+
