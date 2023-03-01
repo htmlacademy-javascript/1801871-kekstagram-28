4fb7011a4de4dc4f488347a89a1b750baa1c470e
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-03-01 11:41:54 +0300

    Правки по функциям

--- js/functions.js
+++ js/functions.js
@@ -5,29 +5,48 @@ const isStringLengthCorrect = (string, length) => (string.length <= length);
 // Функция для проверки, является ли строка палиндромом
 
 const isPalindrome = (string) => {
-  const stringLowerCaseNoSpace = string.split(' ').join('').toLowerCase();
-  return stringLowerCaseNoSpace === stringLowerCaseNoSpace.split('').reverse().join('');
+  const formattedString = string.split(' ').join('').toLowerCase();
+  return formattedString === formattedString.split('').reverse().join('');
 };
 
 // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа
 
-const getNumberInString = (numberString) => (numberString.toString().replaceAll(/\D/g, '')) ? numberString.toString().replaceAll(/\D/g, '') : NaN;
 
-// Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки. Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.
-const addSymbol = (addableLength, symbols) => {
-  if (symbols.length >= addableLength) {
-    return symbols.slice(0,addableLength);
-  }
-  let addSymbols = '';
-  let howMuchAdd = addableLength;
-  while(addSymbols.length < addableLength) {
-    addSymbols = symbols.slice(0, howMuchAdd) + addSymbols;
-    howMuchAdd = howMuchAdd - symbols.length;
-  }
-  return addSymbols;
+const getNumberInString = (numberString) => {
+  const formattedString = numberString.toString().replaceAll(/\D/g, '');
+  return formattedString ? Number(formattedString) : NaN;
 };
 
-const formatStringToPattern = (string, minLength, symbols) => (string.length >= minLength) ? string : addSymbol(minLength - string.length , symbols) + string;
+// Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки. Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.
+
+
+// const getSymbol = (pattern, length) => {
+//   if (pattern.length >= length) {
+//     return pattern.slice(0, length);
+//   }
+//   let addPattern = '';
+
+//   while(addPattern.length < length - pattern.length) {
+//     addPattern = addPattern + pattern;
+// }
+//   return pattern.slice(0, length - addPattern.length) + addPattern;
+// };
+// Реализация без repeat
+
+const getSymbol = (pattern, length) => {
+  if (pattern.length >= length) {
+    return pattern.slice(0, length);
+  }
+  const addPattern = pattern.repeat(Math.floor(length / pattern.length));
+  return pattern.slice(0, length - addPattern.length) + addPattern;
+};
+
+const formatStringToPattern = (string, minLength, pattern) => {
+  if (string.length >= minLength) {
+    return string;
+  }
+  return getSymbol(pattern , minLength - string.length) + string;
+};
 
 isStringLengthCorrect();
 isPalindrome();
