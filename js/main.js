ff3088987454b45979bedaa40b7a578f1b494fa3
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-03-24 19:01:38 +0300

    Посты

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import {createPosts} from './mockup.js';
+import {createpicturesFragment} from './picture.js';
 
 const NUMBER_OF_POSTS = 25;
 
-createPosts(NUMBER_OF_POSTS);
+const posts = createPosts(NUMBER_OF_POSTS);
+
+const picturesPlace = document.querySelector('.pictures');
+picturesPlace.appendChild(createpicturesFragment(posts));
+
+
