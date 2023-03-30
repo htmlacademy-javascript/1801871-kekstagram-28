ef9f719d72460707aa970f1c3d21514774560cfc
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-03-30 19:07:22 +0300

    Add event listener

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,26 +1,77 @@
+import {isEscapeKey} from './util.js';
 const bigPicture = document.querySelector('.big-picture');
-const bigPictureImg = bigPicture.querySelector('.big-picture__img');
+const socialComment = bigPicture.querySelector('.social__comment');
+const socialCommentList = bigPicture.querySelector('.social__comments');
+const body = document.querySelector('body');
 
 
+const isImgUploadClick = (evt) => evt.target.closest('.img-upload') !== null;
 
-const isUrlSame = (element) => (this.url === element.url);
 
-const getCurrentPictureData = (evt) => {
-  const url = evt.target.querySelector('.picture__img').src;
-  return this.posts.find(isUrlSame);
+const getCurrentPictureData = (posts, evt) => {
+  const picture = evt.target.closest('.picture');
+  const url = picture.querySelector('.picture__img').src;
+  const urlEnd = url.match(/photos\/\d+\.jpg/)[0];
+  return posts.find((element) => urlEnd === element.url);
 };
 
-const renderLargePicture = (evt) => {
-  const pictureData = getCurrentPictureData(evt);
-  bigPictureImg.img.src = pictureData.url;
+const renderLargePicture = (pictureData) => {
+  bigPicture.querySelector('img').src = pictureData.url;
+  bigPicture.querySelector('.social__caption').textContent = pictureData.description;
+  bigPicture.querySelector('.likes-count').textContent = pictureData.likes;
+  bigPicture.querySelector('.social__comment-count').childNodes[0].textContent = pictureData.comments.length < 5 ? `${pictureData.comments.length} из ` : '5 из ';
+  bigPicture.querySelector('.comments-count').textContent = pictureData.comments.length;
 };
-console.log(bigPicture);
 
-const enlargePicture = (evt) => {
-  if (evt.target.nodeName === 'A') {
-    bigPicture.classList.remove('hidden');
-  // renderLargePicture(evt);
+const getComment = (comment) => {
+  const newComment = socialComment.cloneNode(true);
+  newComment.querySelector('img').src = comment.avatar;
+  newComment.querySelector('img').alt = comment.name;
+  newComment.querySelector('.social__text').textContent = comment.message;
+  return newComment;
+};
+
+const createFragmentComments = (pictureData) => {
+  const fragment = document.createDocumentFragment();
+  pictureData.comments.forEach((comment)=>{
+    fragment.append(getComment(comment));
+  });
+  return fragment;
+};
+
+const renderComments = (fragment) => {
+  socialComment.innerHTML = '';
+  const comments = fragment.querySelectorAll('li');
+  if (comments.length < 5) {
+    comments.forEach((element) => {
+      socialCommentList.append(element);
+    });
   }
 };
 
-export {enlargePicture};
+const closePicture = () => {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+};
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closePicture ();
+  }
+};
+
+const enlargePicture = (evt, posts) => {
+  if (!isImgUploadClick(evt)) {
+    bigPicture.classList.remove('hidden');
+    const pictureData = getCurrentPictureData(posts, evt);
+    renderLargePicture(pictureData);
+    renderComments(createFragmentComments(pictureData));
+    body.classList.add('modal-open');
+
+    document.addEventListener('keydown', onDocumentKeydown);
+  }
+};
+
+export {enlargePicture, closePicture};
+
--- js/main.js
+++ js/main.js
@@ -1,39 +1,18 @@
 import {createPosts} from './mockup.js';
 import {getPicturesFragment} from './picture.js';
-// import {enlargePicture} from './big-picture.js';
+import {enlargePicture, closePicture} from './big-picture.js';
 
 const NUMBER_OF_POSTS = 25;
+const bigPictureCancel = document.querySelector('.big-picture__cancel');
 
 const posts = createPosts(NUMBER_OF_POSTS);
 
 const pictureContainer = document.querySelector('.pictures');
+
 pictureContainer.appendChild(getPicturesFragment(posts));
 
-// 1231313123
-const bigPicture = document.querySelector('.big-picture');
-const isImgUploadClick = (evt) => evt.target.closest('.img-upload') !== null;
-// const isUrlSame = (element) => (this.url === element.url);
 
-const getCurrentPictureData = (evt) => {
-  const picture = evt.target.closest('.picture');
-  const url = picture.querySelector('.picture__img').src;
-  const urlUp = url.match(/photos\/\d+\.jpg/)[0];
-  return posts.find((element) => urlUp === element.url);
-};
-
-const renderLargePicture = (evt) => {
-  const pictureData = getCurrentPictureData(evt);
-  bigPicture.querySelector('img').src = pictureData.url;
-  bigPicture.querySelector('.likes-count').textContent = pictureData.likes;
-};
-
-
-
-const enlargePicture = function (evt) {
-  if (!isImgUploadClick(evt)) {
-    bigPicture.classList.remove('hidden');
-    renderLargePicture(evt);
-  }
-};
-
-pictureContainer.addEventListener('click', enlargePicture);
+pictureContainer.addEventListener('click', (evt) => {
+  enlargePicture(evt, posts);
+});
+bigPictureCancel.addEventListener('click', closePicture);
